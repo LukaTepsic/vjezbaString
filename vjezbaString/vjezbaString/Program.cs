851ca59ec45ae55fb04d2b1a1a389836b729e0a7
LukaTepsic <166146790+LukaTepsic@users.noreply.github.com>
2024-10-28 12:45:40 +0100

    Add files via upload

---
+++ vjezbaString/vjezbaString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Unesite string:");
+        string input = Console.ReadLine();
+
+        if (input.Length < 5)
+        {
+            Console.WriteLine(input);
+        }
+        else
+        {
+            string result = input.Substring(2, input.Length - 4);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
